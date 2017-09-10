82ccfe153b7611220e5cfc74439e92fe7ae80139
zglrx <79danil@gmail.com>
2017-09-10 22:29:52 +0300

    Second steps ..

--- MainInfo/models.py
+++ MainInfo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Feedback(models.Model):
+    author = models.CharField(max_length = 254)
+    email = models.EmailField(max_length = 254)
+    phone_number = models.IntegerField(max_length = 254)
+    text = models.TextField(max_length = 2048)
+    created_date = models.DateTimeField(default = timezone.now())
+
+    def publish (self):
+        self.save()
+
+    def __str__(self):
+        return self.text
